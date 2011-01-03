92e991845b31b428c5e27a423ec3b5d002428811
malcolm <malcolm@asana.com>
2011-01-04 00:05:42 +0700

    shortcut opens switcher

--- content.js
+++ content.js
@@ -3,6 +3,7 @@ console.log('content.js is loading');
 var onKeyPress = function(event) {
   if (event.keyCode === 69 && event.metaKey) {
     console.log('onKeyPress');
+    chrome.extension.sendRequest({action: "showPopup"});
   }
 };
 document.body.addEventListener('keydown', onKeyPress, /*useCapture=*/ true);
