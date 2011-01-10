2fceed336419e9e7597c71b916e717a2f8d64d35
malcolm <malcolm@asana.com>
2011-01-10 22:03:17 +0630

    cp

--- content.js
+++ content.js
@@ -2,8 +2,7 @@ console.log('content.js is loading');
 
 var onKeyPress = function(event) {
   if (event.keyCode === 69 && event.metaKey) {
-    console.log('onKeyPress');
     chrome.extension.sendRequest({action: "showPopup"});
   }
 };
-document.addEventListener('keydown', onKeyPress, /*useCapture=*/ true);
+window.addEventListener('keydown', onKeyPress, /*useCapture=*/ true);
