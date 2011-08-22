e0d827ae6707f2311071ed0cadb1638e3bd78dce
malcolm <malcolm@asana.com>
2011-08-21 18:08:24 -0700

    shortcut works in gmail

--- content.js
+++ content.js
@@ -1,8 +1,41 @@
-console.log('content.js is loading');
+var descendentsMatchingXpath = function(parent, xpath) {
+  var iter = document.evaluate(xpath, parent, null, XPathResult.ORDERED_NODE_ITERATOR_TYPE, null);
+  var nodes = [];
+  while (true) {
+    var node = iter.iterateNext();
+    if (node === null) {
+      break;
+    } else {
+      nodes.push(node);
+    }
+  }
+  return nodes;
+};
 
 var onKeyPress = function(event) {
-  if (event.keyCode === 69 && event.metaKey) {
+  if (event.keyCode === 75 && event.metaKey) { // command-K
     chrome.extension.sendRequest({action: "showPopup"});
   }
 };
 window.addEventListener('keydown', onKeyPress, /*useCapture=*/ true);
+
+// Iframes with a src attribute will run their own copy of the
+// extension but frames with no src attribute will not (just like
+// empty pages). Since Gmail's compose view works like this we need to
+// do something about it. Extensions can't get to frame.contentWindow
+// but they can get to frame.contentDocument, which is enough to
+// install a listener.
+//
+// References:
+// - http://code.google.com/p/chromium/issues/detail?id=20773
+// - http://blog.afterthedeadline.com/2010/05/14/how-to-jump-through-hoops-and-make-a-chrome-extension/
+var addListenerToIframes = function() {
+  descendentsMatchingXpath(document, '//iframe[not(@src)]').forEach(function(iframe) {
+      if (iframe.__power_switch_handler_set === undefined) {
+        iframe.contentDocument.addEventListener('keydown', onKeyPress, /*useCapture=*/ true);
+        iframe.__power_switch_handler_set = true;
+      }
+    });
+};
+addListenerToIframes();
+setInterval(addListenerToIframes, 1000);
